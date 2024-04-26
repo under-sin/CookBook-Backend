bb12441a2ca4c291d8e18e6d8c3b9d03e6c896f0
Andy <andersonvieira818@yahoo.com.br>
2024-04-25 21:46:05 -0300

    feat: adding infrastructure project

---
+++ src/Backend/MyRecipeBook.Infrastructure/DataAccess/UnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using MyRecipeBook.Domain.Repositories;
+
+namespace MyRecipeBook.Infrastructure.DataAccess;
+
+public class UnitOfWork : IUnitOfWork
+{
+    private readonly MyRecipeBookDbContext _context;
+
+    public UnitOfWork(MyRecipeBookDbContext context) => _context = context;
+
+    public async Task Commit() => await _context.SaveChangesAsync();
+}
---
+++ src/Backend/MyRecipeBook.Infrastructure/DependencyInjectionExtension.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using MyRecipeBook.Domain.Repositories;
+using MyRecipeBook.Domain.Repositories.Users;
+using MyRecipeBook.Infrastructure.DataAccess;
+using MyRecipeBook.Infrastructure.DataAccess.Repositories;
+
+namespace MyRecipeBook.Infrastructure;
+
+public static class DependencyInjectionExtension
+{
+    // esse método preciso ser um IServiceCollection para que possa ser chamado no Program.cs
+    public static void AddInfrastructure(this IServiceCollection services, IConfiguration configuration)
+    {
+        AddDbContext(services, configuration);
+        AddRepositories(services);
+    }
+
+    private static void AddDbContext(IServiceCollection services, IConfiguration configuration)
+    {
+        // Configurando a conexão com o banco de dados (mysql)
+        var connectionString = configuration.GetConnectionString("Connection");
+        var serverVersion = new MySqlServerVersion(new Version(8, 3, 0));
+
+        services.AddDbContext<MyRecipeBookDbContext>(dbContextOptions =>
+        {
+            dbContextOptions.UseMySql(connectionString, serverVersion);
+        });
+    }
+
+    private static void AddRepositories(IServiceCollection services)
+    {
+        services.AddScoped<IUnitOfWork, UnitOfWork>();
+        
+        services.AddScoped<IUserReadOnlyRepository, UserRepository>();
+        services.AddScoped<IUserWriteOnlyRepository, UserRepository>();
+    }
+}
